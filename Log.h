6209c72ce954c6dc9a05a3d6fbeb91da77ed3446
wangwj-f <wangwj-f@glodon.com>
2019-10-11 17:17:28 +0800

    缺少包含头文件

--- Log.h
+++ Log.h
@@ -1,42 +1,46 @@
 ﻿#ifndef __CLOG_H__
 #define __CLOG_H__
+#pragma once
 
 #include <stdio.h>
 #include <varargs.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 
+#define LOG_MAX_SIZE 128
 namespace Log
 {
-	inline void Debug(char *format, ...)
-	{
-		char buf[128] = {0};
+    inline void Debug(char *format, ...)
+    {
+        char buf[LOG_MAX_SIZE] = { 0 };
 
-		va_list args;
-		va_start(args, format);
-		int len = _vsnprintf_s(buf, _countof(buf)-1, format, args);
-		va_end(args);
+        va_list args;
+        va_start(args, format);
+        int len = _vsnprintf_s(buf, _countof(buf) - 1, format, args);
+        va_end(args);
 
-		if (len != -1)
-		{
-			buf[127] = '\0';
-			OutputDebugStringA(buf);
-		}
-	}
+        if (len != -1)
+        {
+            buf[LOG_MAX_SIZE-1] = '\0';
+            OutputDebugStringA(buf);
+        }
+    }
 
-	inline void DebugW(wchar_t *format, ...)
-	{
-		wchar_t buf[128] = {0};
+    inline void DebugW(wchar_t *format, ...)
+    {
+        wchar_t buf[LOG_MAX_SIZE] = { 0 };
 
-		va_list args;
-		va_start(args, format);
-		int len = _vsnwprintf_s(buf, _countof(buf)-1, format, args);
-		va_end(args);
+        va_list args;
+        va_start(args, format);
+        int len = _vsnwprintf_s(buf, _countof(buf) - 1, format, args);
+        va_end(args);
 
-		if (len != -1)
-		{
-			buf[127] = '\0';
-			OutputDebugStringW(buf);
-		}
-	}
+        if (len != -1)
+        {
+            buf[LOG_MAX_SIZE-1] = '\0';
+            OutputDebugStringW(buf);
+        }
+    }
 }
 
 #ifdef _DEBUG
